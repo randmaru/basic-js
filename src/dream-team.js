ac27daa836642fcd033309e8c2a58dc1c4177785
randmaru <git@randma.ru>
2021-09-13 23:09:34 +0300

    feat: resolved Dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,19 +1,25 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default members => {
+	if (Array.isArray(members)) {
+		let result = members
+			.filter((item) => typeof item === 'string')
+			.map((item) => item.trim())
+			.map((item) => item[0].toUpperCase())
+			.sort()
+			.join('');
+		return result;
+	}
+	return false;
 }
